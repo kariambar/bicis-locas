2f061c34735d9507f2e21a896727c63e0b81862f
kariambar <karina.spalacios@gmail.com>
2017-05-15 13:58:03 -0400

    añadiendo el nombre

--- js/main.js
+++ js/main.js
@@ -1,52 +1,20 @@
-function validateForm(){
-	/* Escribe tú código aquí */
-	var name = document.getElementById('name');
-	var lastName = document.getElementById('lastname');
-	var email = document.getElementById('input-email');
-	var password = document.getElementById('input-password');
 
-	if(Validator.isText(name.value))
-	{
-		removeMessage('name');
-	}else{
-		createMessage('name', 'No es válido');
-	}
+function validateForm(){ 
+	var nombre = document.getElementById('name').value;
+	var apellido = document.getElementById('lastname').value;
+	var email = document.getElementById('input-email').value;
+	var contrasena = document.getElementById('input-password').value;
+	var seleccionar = document.getElementById("form-control").value;
+  
 
-	if(Validator.isText(lastName.value))
-	{
-		removeMessage('lastname');
-	}else{
-		createMessage('lastname', 'Ingresa tu apellido');
-	}
-
-	if(Validator.isValidPassword(password.value))
-	{
-		removeMessage('input-password');
-	}else{
-		createMessage('input-password', 'Tu password no es válido');
-	}
-
-	if(Validator.isEmail(email.value))
-	{
-		removeMessage('input-email');
-	}else{
-		createMessage('input-email', 'Tu e-mail no es válido');
-	}
+//Todos los campos son obligatorios, excepto los dos últimos.
+	if (nombre == "" || nombre.length == 0){
+        var span = document.createElement("span");
+        var contiene = document.getElementById('name');
+        var padre = contiene.parentNode;
+        padre.appendChild(span);
+        var texto = document.createTextNode("Debe ingresar su nombre");
+        nuevoSpan.appendChild(texto);
+        return nuevoSpan;
+    }
 }
-
-
-/*
-function translate(){
-    var title = document.getElementById("form-signin-heading");
-    var email = document.getElementById("inputEmail");
-    var password = document.getElementById("inputPassword");
-    var remenber = document.getElementsByTagName("span")[0];
-    var button = document.getElementsByClassName("btn")[0];
-    title.innerHTML = "Por favor inicia sesión";
-    email.placeholder = "Correo Electrónico";
-    password.placeholder = "Contraseña";
-    remenber.innerHTML = "Recordar datos";
-    button.innerHTML = "Iniciar Sesión";
-}
-translate();
-*/
