adc8dfcb51a929cc352797311b19c93769adfe6d
karina palacios <karina.spalacios@gmail.com>
2017-05-15 21:20:22 -0400

    añadiendo js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
+/*Validando cada campo */
+function validateForm(){
+    var nombre =  document.getElementById("name").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("input-email").value;
+    var password = document.getElementsByClassName("input-password").value;
+    var bici = document.getElementsByClassName("form-group input-box").value;
+    var tuiter = document.getElementById("input-social");
+    var letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-function validateForm(){ 
-	var nombre = document.getElementById('name').value;
-	var apellido = document.getElementById('lastname').value;
-	var email = document.getElementById('input-email').value;
-	var contrasena = document.getElementById('input-password').value;
-	var seleccionar = document.getElementById("form-control").value;
-  
-
-//Todos los campos son obligatorios, excepto los dos últimos.
+/*Campos de validacion Nombre */
 if (nombre == "" || nombre.length == 0){
         var span = document.createElement("span");
         var contiene = document.getElementById("name");
@@ -15,6 +16,57 @@ if (nombre == "" || nombre.length == 0){
         padre.appendChild(span);
         var texto = document.createTextNode("Debe ingresar su nombre");
         span.appendChild(texto);
-        return span;
-    }
+        //return span;
+    }else if( nombre != letter){
+        var span = document.createElement("span");
+        var contiene = document.getElementById("name");
+        var padre = contiene.parentNode;
+        padre.appendChild(span);
+        var texto = document.createTextNode("Debe contener solo letras");
+        span.appendChild(texto);
+        }
+
+   /*Campos de validacion Apellido */ 
+if (lastname == "" || lastname.length == 0 ){
+        var span = document.createElement("span");
+        var contiene = document.getElementById("lastname");
+        var padre = contiene.parentNode;
+        padre.appendChild(span);
+        var texto = document.createTextNode("Debe ingresar su apellido");
+        span.appendChild(texto);
+       // return span;
+}
+
+/*Campos de validacion Email */
+if (email == "" || email == 0 ){
+        var span = document.createElement("span");
+        var contiene = document.getElementById("input-email");
+        var padre = contiene.parentNode;
+        padre.appendChild(span);
+        var texto = document.createTextNode("Debe ingresar su email");
+        span.appendChild(texto);
+       
+}
+
+/*Campos de validacion Contraseña */
+if (password == "" || password == 0 ){
+        var span = document.createElement("span");
+        var contiene = document.getElementById("input-password");
+        var padre = contiene.parentNode;
+        padre.appendChild(span);
+        var texto = document.createTextNode("Debe ingresar su contraseña");
+        span.appendChild(texto);
+       
+}
+
+/*Campos de validacion Bici */
+if (bici == "" || bici == 0 ){
+        var span = document.createElement("span");
+        var contiene = document.getElementById("form-group input-box");
+        var padre = contiene.parentNode;
+        padre.appendChild(span);
+        var texto = document.createTextNode("Debe seleccionar un tipo de bicicleta");
+        span.appendChild(texto);
+       
+}
 }
